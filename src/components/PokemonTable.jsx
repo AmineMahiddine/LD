2b06672a72664455d2b3997ff8164c184d51b4c9
Mohamed Amine Mahiddine <aminemahiddine.io@gmail.com>
2023-09-27 12:20:28 +0100

    version 1.0

--- src/components/PokemonTable.jsx
+++ src/components/PokemonTable.jsx
@@ -1,29 +1,25 @@
-import { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
-import { useTheme } from '@mui/material/styles';
+import { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import { useTheme } from "@mui/material/styles";
 
 import {
   Box,
-  Grid,
   TextField,
   Table,
   TableHead,
   TableBody,
   TableCell,
   TableContainer,
-  TableFooter,
   TablePagination,
   TableRow,
   Paper,
   IconButton,
-} from '@mui/material';
-import {
-  KeyboardArrowLeft,
-  KeyboardArrowRight,
-} from '@mui/icons-material';
-
+  Stack,
+  Typography,
+} from "@mui/material";
+import { KeyboardArrowLeft, KeyboardArrowRight } from "@mui/icons-material";
 import SearchIcon from '@mui/icons-material/Search';
-import AccountCircle from '@mui/icons-material/AccountCircle';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 
 
 function TablePaginationActions(props) {
@@ -40,7 +36,7 @@ function TablePaginationActions(props) {
         disabled={page === 0}
         aria-label="previous page"
       >
-        {theme.direction === 'rtl' ? (
+        {theme.direction === "rtl" ? (
           <KeyboardArrowRight />
         ) : (
           <KeyboardArrowLeft />
@@ -51,7 +47,7 @@ function TablePaginationActions(props) {
         disabled={page >= Math.ceil(count / rowsPerPage) - 1}
         aria-label="next page"
       >
-        {theme.direction === 'rtl' ? (
+        {theme.direction === "rtl" ? (
           <KeyboardArrowLeft />
         ) : (
           <KeyboardArrowRight />
@@ -72,11 +68,11 @@ function PokemonTable() {
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(5);
   const [pokemonData, setPokemonData] = useState([]);
-  const [searchName, setSearchName] = useState('');
+  const [searchName, setSearchName] = useState("");
   const [minPower, setMinPower] = useState(0);
 
   useEffect(() => {
-    fetch('/pokemon.json')
+    fetch("/pokemon.json")
       .then((response) => response.json())
       .then((data) => setPokemonData(data));
   }, []);
@@ -120,100 +116,128 @@ function PokemonTable() {
   };
 
   const emptyRows =
-    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - filteredPokemon.length) : 0;
+    page > 0
+      ? Math.max(0, (1 + page) * rowsPerPage - filteredPokemon.length)
+      : 0;
 
   return (
-    <div>
-  
-        
-        {/* <SearchIcon/> */}
-      <TextField id="outlined-basic" label="Search..." variant="outlined" value={searchName}
-        onChange={(e) => setSearchName(e.target.value)}/>
+    <Stack spacing={3} margin={6} >
+      <Paper
+        sx={{
+          padding: 2,
+          boxShadow: 3,
+        }}
+      >
+        <Stack spacing={1} direction="row" marginBottom={2}>
+          <TextField
+            id="outlined-basic"
+            // label="Search..."
+            placeholder="Search..."
+            variant="outlined"
+            size="small"
+            value={searchName}
+            onChange={(e) => setSearchName(e.target.value)}
+            sx={{
+              flexGrow: 1,
+            }}
+            InputProps={{
+              startAdornment: (
+                  <SearchIcon sx={{ marginRight: '8px'}}/>
+              ),
+            }}
+          />
 
+          <TextField
+            type="number"
+            id="outlined-basic"
+            label="Power threshold"
+            placeholder="Power threshold"
+            variant="outlined"
+            size="small"
+            value={minPower}
+            onChange={(e) => setMinPower(parseInt(e.target.value))}
+            sx={{
+              flexGrow: 1,
+            }}
+            InputProps={{
+              startAdornment: (
+                  <FavoriteBorderIcon sx={{ marginRight: '8px'}}/>
+              ),
+            }}
+          />
+        </Stack>
 
+        <Box>
+          <Typography>Min Power: {currentMinPower}</Typography>
+          <Typography>Max Power: {currentMaxPower}</Typography>
+        </Box>
+      </Paper>
 
-
-  
-      <TextField
-        type="number"
-        id="outlined-basic" label="Power threshold" variant="outlined" value={minPower}
-        onChange={(e) => setMinPower(parseInt(e.target.value))}/>
-        
-      <div>
-        <p>Min Power: {currentMinPower}</p>
-        <p>Max Power: {currentMaxPower}</p>
-      </div>
-     
-      <TableContainer component={Paper}>
-        <Table>
-          <TableHead style={{ backgroundColor: '#F9F9F9' }}>
-            <TableRow>
-              <TableCell align="center">ID</TableCell>
-              <TableCell align="center">Name</TableCell>
-              <TableCell align="center">Type</TableCell>
-              <TableCell align="center">Health</TableCell>
-              <TableCell align="center">Attack</TableCell>
-              <TableCell align="center">Defense</TableCell>
-              <TableCell align="center">Special Attack</TableCell>
-              <TableCell align="center">Special Defense</TableCell>
-              <TableCell align="center">Speed</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody align="center">
-            {(rowsPerPage > 0
-              ? filteredPokemon.slice(
-                  page * rowsPerPage,
-                  page * rowsPerPage + rowsPerPage
-                )
-              : filteredPokemon
-            ).map((pokemon) => (
-              <TableRow key={pokemon.id}>
-                <TableCell align="center" component="th" scope="row">
-                  {pokemon.id}
-                </TableCell>
-                <TableCell align="center" component="th" scope="row">
-                  {pokemon.name}
-                </TableCell>
-                <TableCell align="center" component="th" scope="row">
-                  {pokemon.type.join(', ')}
-                </TableCell>
-                <TableCell align="center">{pokemon.hp}</TableCell>
-                <TableCell align="center">{pokemon.attack}</TableCell>
-                <TableCell align="center">{pokemon.defense}</TableCell>
-                <TableCell align="center">{pokemon.special_attack}</TableCell>
-                <TableCell align="center">{pokemon.special_defense}</TableCell>
-                <TableCell align="center">{pokemon.speed}</TableCell>
+      <Stack alignItems="end">
+        <TableContainer component={Paper} sx={{ boxShadow: 3 }}>
+          <Table>
+            <TableHead style={{ backgroundColor: "#F9F9F9" }}>
+              <TableRow style={{color: "red "}}>
+                <TableCell align="center">ID</TableCell>
+                <TableCell align="center">Name</TableCell>
+                <TableCell align="center">Type</TableCell>
+                <TableCell align="center">Health</TableCell>
+                <TableCell align="center">Attack</TableCell>
+                <TableCell align="center">Defense</TableCell>
+                <TableCell align="center">Special Attack</TableCell>
+                <TableCell align="center">Special Defense</TableCell>
+                <TableCell align="center">Speed</TableCell>
               </TableRow>
-            ))}
-            {emptyRows > 0 && (
-              <TableRow style={{ height: 53 * emptyRows }}>
-                <TableCell colSpan={9} />
-              </TableRow>
-            )}
-          </TableBody>
-          <TableFooter>
-            <TableRow>
-              <TablePagination
-                rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
-                colSpan={9}
-                count={filteredPokemon.length}
-                rowsPerPage={rowsPerPage}
-                page={page}
-                SelectProps={{
-                  inputProps: {
-                    'aria-label': 'rows per page',
-                  },
-                  native: true,
-                }}
-                onPageChange={handleChangePage}
-                onRowsPerPageChange={handleChangeRowsPerPage}
-                ActionsComponent={TablePaginationActions}
-              />
-            </TableRow>
-          </TableFooter>
-        </Table>
-      </TableContainer>
-    </div>
+            </TableHead>
+            <TableBody align="center">
+              {(rowsPerPage > 0
+                ? filteredPokemon.slice(
+                    page * rowsPerPage,
+                    page * rowsPerPage + rowsPerPage
+                  )
+                : filteredPokemon
+              ).map((pokemon) => (
+                <TableRow key={pokemon.id}>
+                  <TableCell align="center" scope="row">{pokemon.id}</TableCell>
+                  <TableCell align="center" scope="row">{pokemon.name}</TableCell>
+                  <TableCell align="center" scope="row">{pokemon.type.join(", ")}</TableCell>
+                  <TableCell align="center">{pokemon.hp}</TableCell>
+                  <TableCell align="center">{pokemon.attack}</TableCell>
+                  <TableCell align="center">{pokemon.defense}</TableCell>
+                  <TableCell align="center">{pokemon.special_attack}</TableCell>
+                  <TableCell align="center">{pokemon.special_defense}</TableCell>
+                  <TableCell align="center">{pokemon.speed}</TableCell>
+                </TableRow>
+              ))}
+              {emptyRows > 0 && (
+                <TableRow style={{ height: 53 * emptyRows }}>
+                  <TableCell colSpan={9} />
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 25, { label: "All", value: -1 }]}
+          colSpan={9}
+          count={filteredPokemon.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          SelectProps={{
+            inputProps: {
+              "aria-label": "rows per page",
+            },
+            native: true,
+          }}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+          ActionsComponent={TablePaginationActions}
+          sx={{
+            border: "none",
+          }}
+        />
+      </Stack>
+    </Stack>
   );
 }
 
